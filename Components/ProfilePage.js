e6fde67758982ceaffd0e1ba2025413f38647bbd
BradleyHsu <71233990+BradleyHsu@users.noreply.github.com>
2023-04-24 20:38:53 -0500

    Revert "Revert "Vraj branch 2"" (#25)
    Co-authored-by: benpovich <54565075+benpovich@users.noreply.github.com>

--- Components/ProfilePage.js
+++ Components/ProfilePage.js
@@ -1,7 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { StackActions } from '@react-navigation/native';
 import * as db_operations from '../db_operations.js';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
+import { MenuProvider } from 'react-native-popup-menu';
 
 const ProfilePage = ({ navigation, route }) => {
   const username = route.params.username
@@ -58,108 +66,219 @@ const ProfilePage = ({ navigation, route }) => {
   };
 
   return (
-    <View style={styles.container}>
-       <TouchableOpacity onPress={() => navigation.dispatch(StackActions.pop(1))}>
-          <View style={styles.backArrowContainer}>
-            <Image source={require('../assets/icons/back_arrow_icon.png')}
-              style={styles.backArrow} />
-          </View>
-        </TouchableOpacity>
-      <View style={styles.profilePicContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('EditProfile', {
-                                          username: username, 
-                                          current_username: current_username,
-                                          isDefaultUser: false,
-                                        })}>
-          <Image
-            source={{uri: "data:image/png;base64," + profilePicture}}
-            style={styles.profilePicture}
-          />
-        </TouchableOpacity>
+    <MenuProvider>
+      <View style={styles.container}>
+        {username === current_username &&
+          (<View>
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+              <View style={styles.threeDotsContainer}>
+                <Image source={require('../assets/icons/threedots_icon.png')}
+                  style={styles.threeDots} />
+              </View>
+            </TouchableOpacity>
+            <View style={styles.profilePicContainer}>
+                <Image
+                    source={{uri: "data:image/png;base64," + profilePicture}}
+                    style={styles.profilePicture}
+                />
+            </View>
+            <View style={styles.usernameContainer}>
+              <Text style={styles.username}>{name}</Text>
+            </View>
+            <View style={styles.itemsContainer}>
+              <View style={styles.followersContainer}>
+                <Text style={styles.followers}>Followers</Text>
+              </View>
+              <View style={styles.followingContainer}>
+                <Text style={styles.following}>Following</Text>
+              </View>
+              <View style={styles.charmaContainer}>
+                <Text style={styles.charma}>Charma</Text>
+              </View>
+            </View>
+            <View style={styles.itemsNumContainer}>
+              <View style={styles.followersNumContainer}>
+                <Text style={styles.followersNum}>76</Text>
+              </View>
+              <View style={styles.followingNumContainer}>
+                <Text style={styles.followingNum}>142</Text>
+              </View>
+              <View style={styles.charmaNumContainer}>
+                <Text style={styles.charmaNum}>{likes}</Text>
+              </View>
+            </View>
+            <View style={styles.bioContainer}>
+              <View style={styles.bioContainerBold} >
+                <Text style={styles.bio}>
+                  Bio
+                </Text>
+              </View>
+              <View style={styles.bioTextContainer}>
+                <Text style={styles.bioText}>
+                  i am a cool person.
+                </Text>
+              </View>
+            </View>
+            <View style={styles.locationContainer}>
+              <View style={styles.locationTextContainer}>
+                <Text style={styles.locationText}>
+                  Location
+                </Text>
+              </View>
+              <View style={styles.locationInfoContainer}>
+                <Text style={styles.locationInfo}>los angeles, ca</Text>
+              </View>
+            </View>
+          </View>)
+        }
+        {username != current_username &&
+          (
+            <View style={styles.container2}>           
+              <TouchableOpacity onPress={() => navigation.dispatch(StackActions.pop(1))}>
+                <View style={styles.backArrowContainer}>
+                  <Image source={require('../assets/icons/back_arrow_icon.png')}
+                    style={styles.backArrow} />
+                </View>
+              </TouchableOpacity>
+              <View style={styles.username2Container}>
+                <Text style={styles.username2}>{name}</Text>
+              </View>
+              <View style={styles.topRowContainer}>
+                <View style={styles.picContainer}>
+                  <Image source={require('../assets/images/dog_picture.jpg')}
+                    style={styles.pic} />
+                </View>
+                <View style={styles.bottomRowContainer}>
+                  <View style={styles.firstRowContainer}>
+                    <View style={styles.followers2Container}>
+                      <Text style={styles.followers2}>Followers</Text>
+                    </View>
+                    <View style={styles.following2Container}>
+                      <Text style={styles.following2}>Following</Text>
+                    </View>
+                    <View style={styles.charma2Container}>
+                      <Text style={styles.charma2}>Charma</Text>
+                    </View>
+                  </View>
+                  <View style={styles.secondRowContainer}>
+                    <View style={styles.followers2CountContainer}>
+                      <Text style={styles.followingCount2}>20</Text>
+                    </View>
+                    <View style={styles.following2CountContainer}>
+                      <Text style={styles.followersCount2}>40</Text>
+                    </View>
+                    <View style={styles.charma2CountContainer}>
+                      <Text style={styles.charma2Count}>{likes}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.bioWholeContainer}>
+                <View style={styles.bioFrontContainer}>
+                  <Text style={styles.bioFront}>
+                    Bio
+                  </Text>
+                </View>
+                <View style={styles.bioInfoContainer}>
+                  <Text style={styles.bioInfo}>
+                    i am cool person. i love 437.
+                    #washu437rocks
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.mutualFriendsContainer}>
+                <View style={styles.picMutualFriendsContainer}>
+                  <View style={styles.picFriendsContainer}>
+                    <Image source={require('../assets/images/dog_picture.jpg')}
+                      style={styles.picMutualFriends} />
+                  </View>
+                  <View style={styles.moreMutualFriendsContainer}>
+                    <View style={styles.moreMutualFriendsText}>
+                      <Text style={styles.moreMutualFriends}>+18</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={styles.mutualFriendsTextContainer}>
+                  <Text style={styles.mutualFriendsText}>
+                    Mutual friends with [username1] and 18 more
+                  </Text>
+                </View>
+              </View>
+              <TouchableOpacity
+                onPress={handleFollow}
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  backgroundColor: isFollowing ? '#7456C9' : '#5C64B0',
+                  padding: 13,
+                  borderRadius: 15,
+                  marginTop: 7,
+                  marginLeft: 25,
+                  marginRight: 25,
+                }}
+              >
+                <Text style={{
+                  color: '#DFDFDF',
+                  textAlign: 'center',
+                  fontFamily: 'InriaSans-Bold',
+                }}>{isFollowing ? 'Friends' : 'Follow'}</Text>
+              </TouchableOpacity>
+              <View style={styles.postForDay}>
+                <View style={styles.headerContainer}>
+                  <View style={styles.headerLocationContainer}>
+                    <Text style={styles.headerLocation}>Los Angeles, California</Text>
+                  </View>
+                  <View style={styles.headerDotContainer}>
+                    <Text style={styles.headerDot}> • </Text>
+                  </View>
+                  <View style={styles.headerTimeContainer}>
+                    <Text style={styles.headerTime}>2 hrs late</Text>
+                  </View>
+                  <View style={styles.headerThreeDotsContainer}>
+                    <Menu>
+                      <MenuTrigger text='•••' customStyles={styles.threeDots} />
+                      <MenuOptions>
+                        <MenuOption onSelect={() => alert(`Saved`)} text='Save' />
+                        <MenuOption onSelect={() => alert(`Reported`)} >
+                          <Text style={{ color: 'red' }}>Report</Text>
+                        </MenuOption>
+                      </MenuOptions>
+                    </Menu>
+                  </View>
+                </View>
+                <View style={styles.mainTextContainer}>
+                  <Text style={styles.mainText}>Going skydiving...</Text>
+                </View>
+                <View style={styles.footerContainer}>
+                  <View style={styles.upvoteContainer}>
+                    <Image source={require('../assets/icons/black_thumb_icon.png')}
+                      style={styles.upvote} />
+                  </View>
+                  <View style={styles.upvoteNumContainer}>
+                    <Text style={styles.upvoteNum}>20</Text>
+                  </View>
+                  <View style={styles.commentContainer}>
+                    <Image source={require('../assets/icons/comments_icon.png')}
+                      style={styles.comment} />
+                  </View>
+                  <View style={styles.commentNumContainer}>
+                    <Text style={styles.commentNum}>2</Text>
+                  </View>
+                </View>
+              </View>
+            </View>)
+        }
       </View>
-      <View style={styles.usernameContainer}>
-        <Text style={styles.username}>{name}</Text>
-      </View>
-      <View style={styles.itemsContainer}>
-        <View style={styles.followersContainer}>
-          <Text style={styles.followers}>Followers</Text>
-        </View>
-        <View style={styles.followingContainer}>
-          <Text style={styles.following}>Following</Text>
-        </View>
-        <View style={styles.charmaContainer}>
-          <Text style={styles.charma}>Charma</Text>
-        </View>
-      </View>
-      <View style={styles.itemsNumContainer}>
-        <View style={styles.followersNumContainer}>
-          <Text style={styles.followersNum}>76</Text>
-        </View>
-        <View style={styles.followingNumContainer}>
-          <Text style={styles.followingNum}>142</Text>
-        </View>
-        <View style={styles.charmaNumContainer}>
-          <Text style={styles.charmaNum}>{likes}</Text>
-        </View>
-      </View>
-      <View style={styles.bioContainer}>
-        <View style={styles.bioContainerBold} >
-          <Text style={styles.bio}>
-            Bio
-          </Text>
-        </View>
-        <View style={styles.bioTextContainer}>
-          <Text style={styles.bioText}>
-            i am a cool person.
-          </Text>
-        </View>
-      </View>
-      <View style={styles.locationContainer}>
-        <View style={styles.locationTextContainer}>
-          <Text style={styles.locationText}>
-            Location
-          </Text>
-        </View>
-        <View style={styles.locationInfoContainer}>
-          <Text style={styles.locationInfo}>los angeles, ca</Text>
-        </View>
-      </View>
-      {/* <View style={styles.memoriesContainer}>
-        <View style={styles.urMemoriesContainer}>
-          <Text style={styles.urMemories}>Your Memories</Text>
-        </View>
-        <View style={styles.visibleContainer}>
-          <Text style={styles.visible}>(visible only to you)</Text>
-        </View>
-      </View> */}
-      {username === current_username &&
-        (<TouchableOpacity
-          onPress={handleLogout}
-        // style={{ backgroundColor: 'red', padding: 10, borderRadius: 5, marginTop: 20 }}
-        >
-          {/* <Text>Log out</Text> */}
-        </TouchableOpacity>)
-      }
-      {username != current_username &&
-        (<TouchableOpacity
-          onPress={handleFollow}
-        // style={{
-        //   backgroundColor: isFollowing ? 'gray' : 'blue',
-        //   padding: 10,
-        //   borderRadius: 5,
-        //   marginTop: 20,
-        // }}
-        >
-          <Text>{isFollowing ? 'Unfollow' : 'Follow'}</Text>
-        </TouchableOpacity>)
-      }
-    </View>
+    </MenuProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: 'white',
   },
   profilePicContainer: {
     marginTop: 90,
@@ -331,6 +450,244 @@ const styles = StyleSheet.create({
     top: 50,
     left: 20,
     backgroundColor: 'lightgray', //TODO, change this
+  },
+  backArrow: {
+    width: 35,
+    height: 35,
+  },
+  backArrowContainer: {
+    marginLeft: 20,
+    marginTop: 50,
+  },
+  username2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 24,
+    color: '#464646',
+  },
+  username2Container: {
+    position: 'absolute',
+    top: 53,
+    right: 140,
+  },
+  topRowContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  picContainer: {
+    marginLeft: 35,
+  },
+  pic: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  bottomRowContainer: {
+    flexDirection: 'column',
+  },
+  firstRowContainer: {
+    flexDirection: 'row',
+    marginTop: 23,
+  },
+  secondRowContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  followers2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 14,
+    color: '#616161',
+  },
+  following2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 14,
+    color: '#616161',
+  },
+  charma2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 14,
+    color: '#616161',
+  },
+  followers2Container: {
+    marginLeft: 23,
+  },
+  following2Container: {
+    marginLeft: 15,
+  },
+  charma2Container: {
+    marginLeft: 17,
+  },
+  followers2CountContainer: {
+    marginLeft: 43,
+  },
+  following2CountContainer: {
+    marginLeft: 65,
+  },
+  charma2CountContainer: {
+    marginLeft: 59,
+  },
+  followingCount2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 13,
+    color: '#969696',
+    opacity: 0.52,
+  },
+  followersCount2: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 13,
+    color: '#969696',
+    opacity: 0.52,
+  },
+  charma2Count: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 13,
+    color: '#969696',
+    opacity: 0.52,
+  },
+  bioWholeContainer: {
+    marginTop: 15,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  bioFront: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 17,
+    color: '#616161',
+  },
+  bioInfoContainer: {
+    marginTop: 7,
+    marginLeft: 10,
+  },
+  bioInfo: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 13,
+    color: '#969696',
+  },
+  mutualFriendsContainer: {
+    flexDirection: 'row',
+    marginTop: 25,
+    marginLeft: 45,
+  },
+  picMutualFriendsContainer: {
+    flexDirection: 'row',
+  },
+  picFriendsContainer: {
+    zIndex: 100,
+    marginRight: -10,
+  },
+  picMutualFriends: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  moreMutualFriendsContainer: {
+    zIndex: 80,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 15,
+    width: 30,
+    height: 30,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  moreMutualFriendsText: {
+    marginTop: 9,
+    marginLeft: 9,
+  },
+  moreMutualFriends: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 8,
+    color: '#454545',
+    textAlign: 'center',
+  },
+  mutualFriendsTextContainer: {
+    marginRight: 30,
+    marginLeft: 10,
+    marginTop: 7,
+  },
+  mutualFriendsText: {
+    fontFamily: 'InriaSans-Bold',
+    fontSize: 11,
+    color: '#969696',
+  },
+  postForDay: {
+    backgroundColor: '#F1F1F1',
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    height: 200,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    marginLeft: 15,
+    marginTop: 10,
+  },
+  headerLocationContainer: {
+    marginTop: 2,
+  },
+  headerLocation: {
+    fontFamily: 'InriaSans-LightItalic',
+    color: '#9D9D9D',
+    fontSize: 11,
+  },
+  headerDotContainer: {
+
+  },
+  headerDot: {
+    color: '#9B9B9B',
+  },
+  headerTimeContainer: {
+    marginTop: 2,
+  },
+  headerTime: {
+    fontFamily: 'InriaSans-LightItalic',
+    color: '#BDBCBC',
+    fontSize: 10,
+  },
+  headerThreeDotsContainer: {
+    opacity: 0.6,
+    marginLeft: 120,
+  },
+  mainTextContainer: {
+    marginTop: 10,
+    marginLeft: 8,
+  },
+  mainText: {
+    fontFamily: 'InriaSans-Bold',
+    color: '#616161',
+    fontSize: 18,
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    marginTop: 110,
+    marginLeft: 20,
+  },
+  upvoteContainer: {
+    marginRight: 6,
+  },
+  upvote: {
+    height: 20,
+    width: 20,
+    resizeMode: 'contain',
+    opacity: 0.65,
+  },
+  upvoteNumContainer: {
+    marginRight: 12,
+  },
+  upvoteNum: {
+    color: '#726D6D',
+    fontSize: 14,
+  },
+  commentContainer: {
+    marginRight: 6,
+  },
+  comment: {
+    width: 19,
+    height: 19,
+    resizeMode: 'contain',
+    opacity: 0.65,
+  },
+  commentNum: {
+    color: '#726D6D',
+    fontSize: 14,
   },
 });
 
